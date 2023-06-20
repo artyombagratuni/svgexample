524874bee858960851fe5459f734893c97067acd
artyom.bagratuni <artyom.bagratuni@picsart.academy>
2023-06-20 18:40:33 +0400

    Deployed Web site

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
 
 function App() {
+  async function toBlob(svgUrl) {
+    const response = await fetch(svgUrl);
+    const svgText = await response.text();
+
+    const blob = new Blob([svgText], { type: 'image/svg+xml' });
+    const blobUrl = URL.createObjectURL(blob);
+    download(blobUrl);
+  }
+
+  function download(dataUrl) {
+    const link = document.createElement('a');
+    link.href = dataUrl;
+    link.download = 'logo.svg';
+
+    document.body.appendChild(link);
+
+    link.click();
+
+    document.body.removeChild(link);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <button onClick={() => toBlob('https://aicdn.picsart.com/62e3eecd-556a-42e2-b8a8-83a58541b72d_3')}>download</button>
     </div>
   );
 }
